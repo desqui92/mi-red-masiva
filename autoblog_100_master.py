b33bdcb19d56cd6d79547b4ef4cbc20e442d667a
desqui92 <43731462+desqui92@users.noreply.github.com>
2026-08-30 21:21:16 -0300

    Update autoblog_100_master.py

--- autoblog_100_master.py
+++ autoblog_100_master.py
@@ -34,7 +34,7 @@ GITHUB_TOKEN = os.environ["GITHUB_TOKEN"]
 YOUTUBE_COOKIES = os.environ.get("YOUTUBE_COOKIES")  # Se agrega esta línea
 REPO_NAME = os.environ.get("REPO_NAME", "desqui92/mi-red-masiva")
 
-MODEL_GEMINI = "gemini-2.5-flash"
+MODEL_GEMINI = "gemini-3.6-flash"
 REGISTRO_FILE = "procesados.json"
 
 PROPELLER_SCRIPT = """<script>(function(s){s.dataset.zone='11689215',s.src='https://n6wxm.com/vignette.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))</script>"""
